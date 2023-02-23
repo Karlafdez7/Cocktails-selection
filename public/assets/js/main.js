75e5ef0ac21f1ade759da0508002337dffe4ee85
Karla <karlafernandezchirinos@gmail.com>
2023-02-23 18:49:19 +0100

    add HTML-basic__JS-variables

---
+++ public/assets/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const userSearch=document.querySelector(".js-inputSearch");
+
+const btnSearchUser=document.querySelector(".js-btnSearch");
+
+const btnResetUser=document.querySelector(".js-btnReset");
+
+const listFavourites=document.querySelector(".js-favourites");
+
+const listCocktails=document.querySelector(".js-cocktails")
+
+
+//# sourceMappingURL=main.js.map
